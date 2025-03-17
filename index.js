e9e051400fbd21d21c559b706975df4806b67d99
AsaiToshiya <to.asai.60@gmail.com>
2025-03-17 12:30:04 +0900

    path.parse(...).name.basename is not a function を修正する

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const filePath = process.argv[2];
 const markdown = fs.readFileSync(filePath, "utf8");
 const identifier = path
   .parse(filePath)
-  .name.basename(filePath)
-  .toLowerCase()
+  .name.toLowerCase()
   .replace(/[^a-z0-9-]+/g, "-")
   .replace(/-+/g, "-")
   .replace(/^-+|-+$/g, "");
